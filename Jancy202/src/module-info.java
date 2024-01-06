8b84579485afae02bd96913721009f32cfeb8959
Jancy Furtado <jancyfurtado@gmail.com>
2024-01-06 11:20:12 +0530

    first git hub commit

---
+++ Jancy202/src/module-info.java
@@ -0,0 +1,2 @@
+module Jancy202 {
+}
---
+++ Jancy202/src/sybcaB202/Practice.java
@@ -0,0 +1,13 @@
+package sybcaB202;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World, Im Sybca B");
+		
+
+	}
+
+}
