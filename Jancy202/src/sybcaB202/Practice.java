7de4d9277f486524b6ebf8765d378e8f387dccaf
Jancy Furtado <jancyfurtado@gmail.com>
2024-01-13 10:17:22 +0530

    Commit Branch

--- Jancy202/src/sybcaB202/Practice.java
+++ Jancy202/src/sybcaB202/Practice.java
@@ -5,9 +5,12 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World, Im Sybca B");
-		
+		System.out.println("Hello World, Im Sybca B");		
 
 	}
 
+	void display()
+	{
+		System.out.println("Hello Branch");
+	}
 }
