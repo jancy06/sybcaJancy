801aeab132cfae5a11f857060803badddae507d1
Jancy Furtado <jancyfurtado@gmail.com>
2024-01-20 10:05:38 +0530

    Assignment_commit

--- Jancy202/src/sybcaB202/AddNum.java
+++ Jancy202/src/sybcaB202/AddNum.java
@@ -16,7 +16,15 @@ public class AddNum {
 		sum=num1+num2;
 		System.out.println("The sum of two numbers is: "+sum);
 		
-
 	}
+	
+	void substract() {
+		int num1=10;
+		int num2=20;
+		int sum=num1-num2;
+		System.out.println("The sum of two numbers is: "+sum);
+		
+	}
+	
 
 }
