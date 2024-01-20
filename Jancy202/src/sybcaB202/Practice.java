228e50b1689189e9b54afb20c5b9b83383e712ad
Jancy Furtado <jancyfurtado@gmail.com>
2024-01-20 09:42:21 +0530

    previouscommit

--- Jancy202/src/sybcaB202/Practice.java
+++ Jancy202/src/sybcaB202/Practice.java
@@ -5,9 +5,10 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World, Im Sybca B");
-		
+		System.out.println("Hello World, Im Sybca B");	
 
 	}
+	
+	
 
 }
